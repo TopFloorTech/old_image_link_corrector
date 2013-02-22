71ff9b9cd4cec89c178dde7d697774111139e769
István Palócz <istvan@palocz.hu>
2013-02-22 07:35:29 +0100

    The module.

---
+++ tests/run.sh
@@ -0,0 +1,6 @@
+while true
+do
+  clear
+  phpunit test.php
+  sleep 1
+done
---
+++ tests/test.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../old_image_link_corrector.module';
+class LinkExtractorTest extends PHPUnit_Framework_TestCase
+{
+  public function testEmptyString()
+  {
+    $matches = old_image_link_corrector_parse_text('');
+    $this->assertEquals(NULL, $matches);
+  }
+  public function testOneShortImageLink()
+  {
+    $full_link = '/styles/style1/public/insert/test.png';
+    $matches = old_image_link_corrector_parse_text($full_link);
+    $this->assertEquals(1, count($matches));
+    $this->assertEquals($full_link, $matches[0]['full_link']);
+    $this->assertEquals('style1', $matches[0]['style_name']);
+    $this->assertEquals('public', $matches[0]['scheme']);
+    $this->assertEquals('insert/test.png', $matches[0]['path']);
+  }
+  public function testOneFullImageLink()
+  {
+    $full_link = '/sites/default/files/styles/style1/public/insert/test.png';
+    $matches = old_image_link_corrector_parse_text($full_link);
+    $this->assertEquals(1, count($matches));
+    $this->assertEquals($full_link, $matches[0]['full_link']);
+  }
+  public function testProcessOnlyLink() {
+    $text = '/sites/default/files/styles/style1/public/insert/test.png';
+    $this->assertEquals('REPLACE', _old_image_link_corrector_process($text));
+  }
+  public function testProcessOneLinkWhitOtherText() {
+    $text = 'img="/sites/default/files/styles/style1/public/insert/test.png"';
+    $this->assertEquals('img="REPLACE"', _old_image_link_corrector_process($text));
+  }
+  public function testProcessLinksWhitOtherText() {
+    $text = 'text img="/sites/default/files/styles/style1/public/insert/test.png" text img="/sites/default/files/styles/style1/public/insert/test.png" text';
+    $this->assertEquals('text img="REPLACE" text img="REPLACE" text', _old_image_link_corrector_process($text));
+  }
+  public function testProcessFullURL() {
+    $text = 'img="http://example.com/sites/default/files/styles/style1/public/insert/test.png"';
+    $this->assertEquals('img="REPLACE"', _old_image_link_corrector_process($text));
+  }
+}
+/**
+ * Mock image_style_url() function.
+ */
+function image_style_url() {
+  return 'REPLACE';
+}
